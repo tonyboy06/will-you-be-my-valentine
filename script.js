4016d0a615a28dde2ff62f7b4d019856207cc5ae
Joey Cruz <typicaltony0824@gmail.com>
2024-02-14 02:20:32 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let yesBtn = document.getElementById('yes');
+let noBtn = document.getElementById('no');
+let btnBlock = document.getElementById('btnBlock');
+let text = document.getElementById('text');
+let image = document.getElementById('image');
+
+
+
+
+let fontSize = window.getComputedStyle(yesBtn).fontSize;
+
+noBtn.addEventListener('click', function() {
+    fontSize = yesBtn.style.fontSize = parseInt(fontSize) + 10 + "px";
+    console.log(fontSize);
+});
+
+yesBtn.addEventListener('click', function() {
+    btnBlock.style.display = "none";
+    image.src = 'yey.gif'
+    text.textContent = 'Yaaaayyy!! I love You!'
+});
